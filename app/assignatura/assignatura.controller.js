a2c18dbba6b436db3e3d079c567b38a6d361923a
j-aidle <jordivega@iesebre.com>
2019-11-18 12:26:51 +0100

    get i post assignatures

--- app/assignatura/assignatura.controller.js
+++ app/assignatura/assignatura.controller.js
@@ -1,6 +1,46 @@
 angular.module('assignatura')
-    .controller('assignaturaController', ['$scope', 'assignatures', function assignaturaController($scope, assignatures) {
+    .controller('assignaturaController',
+    ['$scope', 'assignatures',
+        function assignaturaController($scope, assignatures) {
+            $scope.loading = false;
 
+            var resetformAssignatures = function () {
+                $scope.novaAssignaturaForm.$setPristine();
+                $scope.novaAssignaturaForm.nom.$touched = false;
+                $scope.novaAssignaturaForm.nom.$untouched = true;
+            };
 
+            assignatures.get().then((response) => {
+                console.log(response);
+                $scope.assignatures = response.data;
+            }, (error) => {
+                console.log(error);
+                }
+            );
 
-    }]);
+            $scope.postassignatura = (isValid) => {
+                if (isValid) {
+                    $scope.loading = true;
+                    var data = {
+                        nom: $scope.assignatures.nom
+                    };
+                    var config = {
+                        headers: { 'Content-Type': 'application/json' }
+                    };
+                    assignatures.post(data, config).then(() => {
+                        $scope.msg = "creat";
+                        assignatures.get().then((response) => {
+                            console.log(response);
+                            $scope.assignatures = response.data;
+                        }, (error) => {
+                            console.log(error);
+                        }
+                        );
+                        resetformAssignatures();
+                        $scope.loading = false;
+                    }, (error) => { console.log(error);});
+
+                }
+            };
+
+        }]);
--- app/serveis/assignatures.factory.js
+++ app/serveis/assignatures.factory.js
@@ -3,6 +3,9 @@ angular.module('serveis')
         let actions = {
             get: () => {
                 return $http.get('http://localhost:50297/api/assignaturas');
+            },
+            post: (data, config) => {
+                return $http.post('http://localhost:50297/api/assignaturas/', data, config);
             }
         };
         return actions;
