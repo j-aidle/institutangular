7b97115a61bfc9ba87085104c6b4cd0448b565db
j-aidle <jordivega@iesebre.com>
2019-11-21 11:28:36 +0100

    update en errors

--- app/professor/professor.controller.js
+++ app/professor/professor.controller.js
@@ -4,11 +4,25 @@ angular.module('professor')
 
         $scope.loading = false;
         $scope.loadingdelete = null;
+        $scope.loadingupdate = null;
 
+        $scope.editing = null;
         $scope.canviProfessor = {
              nom: "",
              cognom: "",
              edat: ""
+        };
+
+        $scope.unselect = () => {
+            $scope.editing = null;
+        };
+
+        $scope.initupdateprofe = (professor, param) => {
+            $scope.editing = professor.id + param;
+            $scope.canviProfessor.id = professor.id;
+            $scope.canviProfessor.nom = professor.nom;
+            $scope.canviProfessor.cognom = professor.cognom;
+            $scope.canviProfessor.edat = professor.edat;
         };
 
 
@@ -18,17 +32,9 @@ angular.module('professor')
             $scope.nouProfeForm.nom.$untouched = true;
             $scope.nouProfeForm.cognom.$touched = false;
             $scope.nouProfeForm.cognom.$untouched = true;
-            $scope.nouProfeForm.Edat.$touched = false;
-            $scope.nouProfeForm.Edat.$untouched = true;
+            $scope.nouProfeForm.edat.$touched = false;
+            $scope.nouProfeForm.edat.$untouched = true;
         }; 
-
-        $scope.initupdatealum = (professor, param) => {
-            $scope.editing = professor.id + param;
-            $scope.canviProfessor.id = professor.id;
-            $scope.canviProfessor.nom = professor.nom;
-            $scope.canviProfessor.cognom = professor.cognom;
-            $scope.canviProfessor.edat = professor.edat;
-        };
 
         professors.get().then((response) => {
             $scope.professors = response.data;
@@ -66,10 +72,42 @@ angular.module('professor')
             };
         };   
 
-            var destroyprofessor = (professor) => {
-                $scope.loadingdelete = professor.id;
-                professors.delete(professor.id, $scope.canviProfessor).then(
-                    () => {
+        var destroyprofessor = (professor) => {
+             $scope.loadingdelete = professor.id;
+             professors.delete(professor.id).then(
+                () => {
+                     professors.get().then((response) => {
+                          $scope.professors = response.data;
+                          console.log(response.data);
+                     }, (error) => {
+                          console.log(error);
+                     });
+                        $scope.loadingdelete = null;
+                    },
+                    (error) => { console.log(error) }
+                );
+        };
+
+
+        $scope.confirmDelete = (ev, professor) => {
+             var confirm = $mdDialog.confirm()
+                 .title('Estas segur que vols esborrar-lo? ID Professor: ' + professor.id)
+                 .textContent('El professor que has seleccionat serà esborrat permanentment.')
+                 .ariaLabel('Esborrar professor')
+                 .targetEvent(ev)
+                 .ok('Si')
+                  .cancel('Cancel');
+              $mdDialog.show(confirm).then(function () {
+                    destroyprofessor(professor);
+              });
+        };
+
+
+            $scope.updateprofessor = (professor) => {
+                $scope.loadingupdate = professor.id;
+                professors.update(professor.id, $scope.canviProfessor).then(
+                    (response) => {
+                        console.log(response);
                         professors.get().then((response) => {
                             $scope.professors = response.data;
                             console.log(response.data);
@@ -77,25 +115,12 @@ angular.module('professor')
                             console.log(error);
                         });
                         $scope.loadingdelete = null;
-                    },
-                    (error) => { console.log(error) }
-                );
-            };
+                        $scope.editing = null;
+                }, (error) => {
+                    console.log(error);
+                });
+            };        
 
-
-        $scope.confirmDelete = (ev, professor) => {
-            var confirm = $mdDialog.confirm()
-                .title('Estas segur que vols esborrar-lo? ID Professor: ' + professor.id)
-                .textContent('El professor que has seleccionat serà esborrat permanentment.')
-                .ariaLabel('Esborrar professor')
-                .targetEvent(ev)
-                .ok('Si')
-                .cancel('Cancel');
-
-            $mdDialog.show(confirm).then(function () {
-                destroyprofessor(professor);
-            });
-        }
 
 
 
--- app/serveis/professors.factory.js
+++ app/serveis/professors.factory.js
@@ -4,11 +4,14 @@ angular.module('serveis')
             get: () => {
                 return $http.get('http://localhost:50297/api/Profes');
             },
-            post: (data,config) => {
-                return $http.post('http://localhost:50297/api/Profes',data,config);
+            post: (data, config) => {
+                return $http.post('http://localhost:50297/api/Profes',data, config);
             },
             delete: (data) => {
                 return $http.delete('http://localhost:50297/api/Profes/' + data);
+            },
+            update: (data, content) => {
+                return $http.put('http://localhost:50297/api/Profes/'+ data, content);
             }
         };
         return actions;
