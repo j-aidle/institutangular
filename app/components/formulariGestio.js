4da2d407b995c8008997f26f33da6d334c52d5cc
j-aidle <jordivega@iesebre.com>
2019-12-02 13:00:27 +0100

    prova component

--- app/components/formulariGestio.js
+++ app/components/formulariGestio.js
@@ -2,8 +2,40 @@ angular.module('gestio')
     .component('formulariGestio', {
         templateUrl: './components/formulariGestio.template.html',
         controller: function formulariGestioController($scope) {
-
-        }
+            $scope.postgestio = (isValid) => {
+                var novaGestio = {
+                    alumneid: "",
+                    professorid: "",
+                    assignaturaid: ""
+                };
+                if (isValid) {
+                    novaGestio = {
+                        alumneid: $scope.novaGestio.alumneid,
+                        professorid: $scope.novaGestio.professorid,
+                        assignaturaid: $scope.novaGestio.assignaturaid,
+                    };
+                    $scope.$emit('created',isValid,novaGestio);
+                    resetForm();
+                }
+                
+            };           
+ 
+           var resetForm = function () {
+                $scope.novaGestio.alumneid = "";
+                $scope.novaGestio.professorid = "";
+                $scope.novaGestio.assignaturaid = "";
+                $scope.novaGestio.$setPristine();
+                $scope.novaGestio.alumneid.$untouched = true;
+                $scope.novaGestio.alumneid.$touched = false;
+                $scope.novaGestio.professorid.$untouched = true;
+                $scope.novaGestio.professorid.$touched = false;
+                $scope.novaGestio.assignaturaid.$untouched = true;
+                $scope.novaGestio.assignaturaid.$touched = false;
+            };  
+    },
+    bindings: {
+        loading: '<'
+    }
     
     
 });
--- app/pages/gestio/gestio.controller.js
+++ app/pages/gestio/gestio.controller.js
@@ -1,5 +1,14 @@
 angular.module('gestio')
-    .controller('gestioController', ['$scope', 'dades', function gestioController($scope, dades) {
+    .controller('gestioController', ['$scope','$mdDialog','$mdToast', 'dades', 
+        function gestioController($scope, $mdDialog,$mdToast,dades) {
+        
+        $scope.loadingCreate = false;
+        $scope.editing = null;
+        
+        //$scope.$on('removed', deletegestio);
+        //$scope.$on('updated', updategestio);
+        $scope.$on('created', postgestio);
+        $scope.$on('refresh', fresh);
 
         dades.get().then(
             (response) => { 
@@ -8,6 +17,101 @@ angular.module('gestio')
                 console.log(error);
             });
 
+        function fresh () {
+            dades.get().then(
+            (response) => { 
+                $scope.dades = response.data;
+                $scope.loadingCreate = false; 
+                $scope.editing = null;
+            }, (error) => {
+                console.log(error);
+            });
+        };
+        fresh();
 
+        function postgestio(event, isValid, novaGestio){
+            if (isValid){
+                $scope.loadingCreate = true;
+                dades.post(novaGestio).then(
+                  (response)=>{
+                      fresh();
+                      ToastCreate();
+                    },(error)=>{
+                        console.log(error);
+                });
+            }
+        }
 
-    }]);
+// TOAST
+
+            var last = {
+                bottom: false,
+                top: true,
+                left: false,
+                right: true
+            };
+
+            var toastPosition = angular.extend({}, last);
+
+            var getToastPosition = function () {
+                sanitizePosition();
+
+                return Object.keys(toastPosition)
+                    .filter(function (pos) {
+                        return toastPosition[pos];
+                    }).join(' ');
+            };
+
+            var sanitizePosition = function () {
+                var current = toastPosition;
+
+                if (current.bottom && last.top) {
+                    current.top = false;
+                }
+                if (current.top && last.bottom) {
+                    current.bottom = false;
+                }
+                if (current.right && last.left) {
+                    current.left = false;
+                }
+                if (current.left && last.right) {
+                    current.right = false;
+                }
+
+                last = angular.extend({}, current);
+            };
+
+            var ToastCreate = function () {
+                var pinTo = getToastPosition();
+
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent('S\'ha creat la matricula!')
+                        .position(pinTo)
+                        .toastClass('md-error')
+                        .hideDelay(3000));
+            };
+
+            var ToastDelete = function () {
+                var pinTo = getToastPosition();
+
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent('S\'ha esborrat la matricula!')
+                        .position(pinTo)
+                        .toastClass('md-error')
+                        .hideDelay(3000));
+            };
+
+            var ToastUpdate = function () {
+                var pinTo = getToastPosition();
+
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent('S\'ha actualitzat la matricula!')
+                        .position(pinTo)
+                        .toastClass('md-error')
+                        .hideDelay(3000));
+            };
+
+}]);
